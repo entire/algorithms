78ce580e8cb5dab6cb186069b505589af15f0933
Kosuke Hata <hata.kosuke@gmail.com>
2020-07-07 14:06:45 -0700

    updating shuffler.h

--- 20200707-easy-shuffle_the_array/Shuffler.h
+++ 20200707-easy-shuffle_the_array/Shuffler.h
@@ -1,8 +1,20 @@
 #include <vector>
+#include <iostream>
 
 class Shuffler {
+    // std::vector<int> nums;
 public:
     std::vector<int> shuffle(std::vector<int>& nums, int n) {
-        
+        std::vector<int> shuffled;
+        for (int i = 0; i < n; ++i) {
+            shuffled.push_back(nums[i]);
+        }
+        return shuffled;
+    }
+
+    void print(std::vector<int>& nums) {
+        for (auto & element : nums) {
+            std::cout << element << std::endl;
+        }
     }
 };
--- 20200707-easy-shuffle_the_array/main.cpp
+++ 20200707-easy-shuffle_the_array/main.cpp
@@ -4,8 +4,10 @@
 
 int main() {
     Shuffler shuffler;
-    std::vector<int> nums = {1, 2, 3, 4, 5};
-    std::vector<int> answer = shuffler.shuffle(nums);
+    std::vector<int> nums = {1,2,3,3,2,1};
+    int index = 3;
+    std::vector<int> answer = shuffler.shuffle(nums, index);
+    shuffler.print(answer);
     return 0;
 
 }
