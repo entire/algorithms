0e3a24982fa0d3c9c667ea227c7b02c1809a6b25
Kosuke Hata <hata.kosuke@gmail.com>
2020-08-06 14:27:59 -0700

    adding more ways to reverse

--- 20200722-reverse-char-array/ReverseString.h
+++ 20200722-reverse-char-array/ReverseString.h
@@ -5,11 +5,33 @@
 class ReverseString
 {
 public:
-    void reverseStringUsingSTL(std::vector<char>& s) {
+    void reverseStringWithChars(std::vector<char>& s) {
         std::reverse(s.begin(), s.end());
-        for (auto& elem : s) {
-            std::cout << elem << " " << std::endl;
+        for (auto i = s.begin(); i != s.end(); ++i)
+            std::cout << *i;
+        std::cout << std::endl;
+    }
+
+    void reverseStringWithString(std::string& s) {
+        std::reverse(s.begin(), s.end());
+        printString(s);
+   }
+
+    void reverseStringBySwap(std::string& s) {
+        // length of string
+        int len = s.length();
+        // second to last of len
+        int n = len - 1;
+        for (int i=0; i<(len/2); ++i) {
+            std::swap(s[i], s[n]);
+            // keep decreasing n until we meet i in the middle
+            n = n-1;
         }
+        printString(s);
+    }
+
+    void printString(std::string& s) {
+        std::cout << s << std::endl;
     }
 };
 
--- 20200722-reverse-char-array/main.cpp
+++ 20200722-reverse-char-array/main.cpp
@@ -1,12 +1,17 @@
 #include <iostream>
 #include <vector>
 #include "ReverseString.h"
+#include <string>
 
 int main() {
     std::vector<char> chars = {'a', 'b', 'c'};
+    std::string greetingString1 = "hello everyone";
+    std::string greetingString2 = "again, hello everyone";
 
     ReverseString reverser;
-    reverser.reverseStringUsingSTL(chars);
+    reverser.reverseStringWithChars(chars);
+    reverser.reverseStringWithString(greetingString1);
+    reverser.reverseStringBySwap(greetingString2);
     return 0;
 
 }
